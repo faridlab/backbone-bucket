aa14878e5aef90fb84fc830ebf85b9fcf69fa365
Farid Hidayat <farid@startapp.id>
2026-05-17 10:39:45 +0700

    chore: regenerate application and infrastructure layers

--- src/application/workflows/mod.rs
+++ src/application/workflows/mod.rs
@@ -1,10 +1,11 @@
 mod file_processing_workflow;
 mod file_upload_workflow;
+mod media_processing_workflow;
 mod multipart_upload_workflow;
 mod share_creation_workflow;
 
-pub use file_processing_workflow::{FileProcessingFlowStatus, FileProcessingFlowStep, FileProcessingFlowInstance, FileProcessingStepHandler, FileProcessingFlowExecutor};
-pub use backbone_core::flow::FlowError;
+pub use file_processing_workflow::{FileProcessingFlowStatus, FileProcessingFlowStep, FileProcessingFlowInstance, FileProcessingStepHandler, FileProcessingFlowExecutor, FlowError};
 pub use file_upload_workflow::{FileUploadFlowStatus, FileUploadFlowStep, FileUploadFlowInstance, FileUploadStepHandler, FileUploadFlowExecutor};
+pub use media_processing_workflow::{MediaProcessingFlowStatus, MediaProcessingFlowStep, MediaProcessingFlowInstance, MediaProcessingStepHandler, MediaProcessingFlowExecutor};
 pub use multipart_upload_workflow::{MultipartUploadFlowStatus, MultipartUploadFlowStep, MultipartUploadFlowInstance, MultipartUploadStepHandler, MultipartUploadFlowExecutor};
 pub use share_creation_workflow::{ShareCreationFlowStatus, ShareCreationFlowStep, ShareCreationFlowInstance, ShareCreationStepHandler, ShareCreationFlowExecutor};
